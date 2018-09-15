7854c78b9caf15263c694263fe3d2878c1c057e2
liuknan <liuknan@gmail.com>
2018-09-15 18:27:59 -0400

    full
    finished

--- main.py
+++ main.py
@@ -15,26 +15,25 @@ API_secret_key = ""
 access_token = ""
 access_token_secret = ""
 
-
 def get_images(screen_name):
     auth = tweepy.OAuthHandler(API_key, API_secret_key)
     auth.set_access_token(access_token, access_token_secret)
     api=tweepy.API(auth)
     alltweets=[]
-    new_tweets = api.user_timeline(screen_name=screen_name, count=10)
+    new_tweets = api.user_timeline(screen_name=screen_name, count=30)
     alltweets.extend(new_tweets)
     oldest = alltweets[-1].id - 1
     while len(new_tweets) > 0:
 
         # all subsiquent requests use the max_id param to prevent duplicates
-        new_tweets = api.user_timeline(screen_name=screen_name, count=10, max_id=oldest)
+        new_tweets = api.user_timeline(screen_name=screen_name, count=30, max_id=oldest,tweet_mode='extended')
 
         # save most recent tweets
         alltweets.extend(new_tweets)
 
         # update the id of the oldest tweet less one
         oldest = alltweets[-1].id - 1
-        if (len(alltweets) > 15):
+        if (len(alltweets) > 30):
             break
         print
         "...%s tweets downloaded so far" % (len(alltweets))
@@ -73,20 +72,23 @@ def download_images(list):
     num = 1
     for n in list:
         nam = str(num)
+        nam=nam.zfill(4)
         file.write(n+'\n')
         n=str(n)
         ##name='./'+str(random.randrange(0,1000))+'.jpg'
         name=n.replace("http://","")
         name=name.replace("/","_")
         img_list.insert(-1,name)
-        dir=os.path.abspath('.')+'/images'
-        if not os.path.exists(dir):
-            os.mkdir(dir)
-        else:
+        ##dir=os.path.abspath('.')+'/images'
+        ##if not os.path.exists(dir):
+            ##os.mkdir(dir)
+        ##else:
 
        ## file_path=os.path.join(dir,n)
-            urllib.request.urlretrieve(n,'img00'+nam+'.jpg')
-            num = num + 1
+        urllib.request.urlretrieve(n,'img'+nam+'.jpg')
+        imgnum=str(num)
+        imgnum_list.insert(-1,'img'+imgnum.zfill(4)+'.jpg')
+        num = num + 1
     file.close()
 
 def video_output():
@@ -94,14 +96,44 @@ def video_output():
     # out_file = 'video_out.mp4'
     # img_data= list
     # video.ins_img(input_file, img_data,out_file)
-    ctrcmd='ffmpeg -i img%003d.jpg test.mp4'
+    ctrcmd='ffmpeg -i img%004d.jpg -pix_fmt yuv420p -r 25 -y test.mp4'
     sp.call(ctrcmd,shell=True)
+
+def image_detection():
+    # Create a Vision client.
+    vision_client = google.cloud.vision.ImageAnnotatorClient()
+
+    # TODO (Developer): Replace this with the name of the local image
+    # file to analyze.
+    i=0
+    for name in imgnum_list:
+
+        image_file_name = imgnum_list[i]
+        with io.open(image_file_name, 'rb') as image_file:
+            content = image_file.read()
+
+    # Use Vision to label the image based on content.
+        image = google.cloud.vision.types.Image(content=content)
+        response = vision_client.label_detection(image=image)
+        im = Image.open(name)
+        draw = ImageDraw.Draw(im)
+        font = ImageFont.truetype('/Users/liuknan/Downloads/Interface/AddOns/Skada/media/fonts/ABF.ttf',32)
+        x,y=(0,0)
+        print('Labels:')
+        for label in response.label_annotations:
+            draw.text((x,y),label.description,fill='red',font=font)
+            y=y+40
+            im.save(imgnum_list[i])
+            print(label.description)
+        i=i+1
+
+
 if __name__ == '__main__':
     get_images('@FoAMortgage')
     get_image_url('tweet.json')
     download_images(same)
     video_output()
-    print(img_list)
+    image_detection()
 ##for status in tweepy.Cursor(api.home_timeline).items(2):
 ##    print (status.txt)
 
